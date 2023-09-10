76f999d720389be4717947639926aec7bf517596
saulprl <phasecross@hotmail.com>
2023-09-10 08:54:52 -0700

    Setup env variables, firebase, and react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,42 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import {
+  AnalyticsProvider,
+  AuthProvider,
+  FirebaseAppProvider,
+  FirestoreProvider,
+} from "reactfire";
 
 import App from "./App.tsx";
 import "./index.css";
+import { firebaseConfig } from "./firebase.ts";
+
+const queryClient = new QueryClient();
+
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+const auth = getAuth(firebase);
+const firestore = getFirestore(firebase);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <FirebaseAppProvider firebaseApp={firebase}>
+        <AnalyticsProvider sdk={analytics}>
+          <AuthProvider sdk={auth}>
+            <FirestoreProvider sdk={firestore}>
+              <App />
+            </FirestoreProvider>
+          </AuthProvider>
+        </AnalyticsProvider>
+      </FirebaseAppProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
